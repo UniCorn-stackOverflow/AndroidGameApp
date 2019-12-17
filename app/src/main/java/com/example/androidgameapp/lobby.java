05b2c8ddc7fa463193e3d4c537b232a7e989a306
Leonard <eichmannleonard@dv-schulen.de>
2019-12-17 11:28:44 +0100

    lobby.java -> hinzugefügt: ImageButton 1 &2 werden angesprochen, unfertige OnclickEvents,
    activity_main.xml -> landscape mode
    activity_lobby.xml -> 3. Button hinzugefügt und landscape gesetzt
    
    AndroidManifest.xml -> "setRequestedOrientation" entfernt

--- app/src/main/java/com/example/androidgameapp/lobby.java
+++ app/src/main/java/com/example/androidgameapp/lobby.java
@@ -2,15 +2,35 @@ package com.example.androidgameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class lobby extends AppCompatActivity {
+
+    private ImageButton imgBtnP1;
+    private ImageButton imgBtnP2;
+    private Button btnChoose;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        imgBtnP1 = (ImageButton)findViewById(R.id.imageButtonPlayer1);
+        imgBtnP2 = (ImageButton)findViewById(R.id.imageButtonPlayer2);
+        btnChoose = (Button) findViewById(R.id.buttonChoose);
+    }
+
+    public void onClickP1(View view) {
+
+    }
+    public void onClickP2(View view) {
+
+    }
+    public void onClickChoose(View view) {
 
     }
 }
