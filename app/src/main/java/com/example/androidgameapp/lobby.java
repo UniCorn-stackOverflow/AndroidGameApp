7e91a73854f0d308b982cc46d180a56faadff971
Leonard <eichmannleonard@dv-schulen.de>
2019-12-17 11:54:00 +0100

    lobby.java -> imageView 1 & 2 haben einen funktionierenden onClick event, der DemoStats der charaktere anzeigt, abhängig vom geclickten ist das richtige offen, wenn eins ausgewählt ist wird der "choose" button verfügbar

--- app/src/main/java/com/example/androidgameapp/lobby.java
+++ app/src/main/java/com/example/androidgameapp/lobby.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class lobby extends AppCompatActivity {
 
     private ImageButton imgBtnP1;
     private ImageButton imgBtnP2;
     private Button btnChoose;
+    private ImageView imgVStatPrev1;
+    private ImageView ImgVStatPrev2;
 
 
 
@@ -22,15 +25,25 @@ public class lobby extends AppCompatActivity {
         imgBtnP1 = (ImageButton)findViewById(R.id.imageButtonPlayer1);
         imgBtnP2 = (ImageButton)findViewById(R.id.imageButtonPlayer2);
         btnChoose = (Button) findViewById(R.id.buttonChoose);
+        imgVStatPrev1 = (ImageView) findViewById(R.id.imageViewStatPreview1);
+        ImgVStatPrev2 = (ImageView) findViewById(R.id.imageViewStatPreview2);
+        btnChoose.setEnabled(false);
     }
 
-    public void onClickP1(View view) {
-
+    public void onClickP1(View view)
+    {
+        imgVStatPrev1.setVisibility(View.VISIBLE);
+        ImgVStatPrev2.setVisibility(View.INVISIBLE);
+        btnChoose.setEnabled(true);
     }
-    public void onClickP2(View view) {
-
+    public void onClickP2(View view)
+    {
+        ImgVStatPrev2.setVisibility(View.VISIBLE);
+        imgVStatPrev1.setVisibility(View.INVISIBLE);
+        btnChoose.setEnabled(true);
     }
-    public void onClickChoose(View view) {
+    public void onClickChoose(View view)
+    {
 
     }
 }
