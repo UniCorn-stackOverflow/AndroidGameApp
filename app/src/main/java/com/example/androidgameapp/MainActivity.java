6d204af4543644a6afb7bdee6d402cdf8c40c5ac
Leonard <eichmannleonard@dv-schulen.de>
2020-01-07 11:26:51 +0100

    activityLVL1 angelegt(unfertig) lobby, intent hinzugefügt, isPlayer1 boolean zu überprüfung hinzugefügt

--- app/src/main/java/com/example/androidgameapp/MainActivity.java
+++ app/src/main/java/com/example/androidgameapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    //diese Zeilke entfrernen
 
         btnStarten = (Button)findViewById(R.id.buttonStartGame);
         //Comment
